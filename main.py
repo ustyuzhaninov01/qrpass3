c4470a41974c336c7bfbfb23324fac01d7124ebc
ustyuzhaninov01 <ustyuzhaninov01@gmail.com>
2024-05-13 19:16:47 -0300

    Initial commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from PIL import Image
 import cv2
 from pyzbar.pyzbar import decode
 import pyqrcode
+import zbar
 
 # Função para verificar se o email está na lista de participantes
 def verificar_participante():
